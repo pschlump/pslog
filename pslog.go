64a3b06d840899fabdb195c0c985f04e734bde5c
Philip Schlump <pschlump@gmail.com>
2019-03-09 12:28:02 -0700

    Initial dummy library.

---
+++ pslog.go
@@ -0,0 +1,25 @@
+package pslog
+
+func Info(x ...interface{}) {
+}
+
+func Infof(x ...interface{}) {
+}
+
+func Warn(x ...interface{}) {
+}
+
+func Warnf(x ...interface{}) {
+}
+
+func Error(x ...interface{}) {
+}
+
+func Errorf(x ...interface{}) {
+}
+
+func Fatal(x ...interface{}) {
+}
+
+func Fatalf(x ...interface{}) {
+}
