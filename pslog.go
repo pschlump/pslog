b77b3f0a70f80a2c3ffcd82c94ce4b3d8b0afd9f
Philip Schlump <pschlump@gmail.com>
2019-03-10 19:34:42 -0600

    1st lame implemenation to get output.

--- pslog.go
+++ pslog.go
@@ -1,25 +1,46 @@
 package pslog
 
+import (
+	"fmt"
+	"os"
+)
+
 func Info(x ...interface{}) {
+	Infof("%s\n", x...)
 }
 
-func Infof(x ...interface{}) {
+func Infof(fmt string, x ...interface{}) {
+	msg("Info:"+fmt, x...)
 }
 
 func Warn(x ...interface{}) {
+	Warnf("%s\n", x...)
 }
 
-func Warnf(x ...interface{}) {
+func Warnf(fmt string, x ...interface{}) {
+	msg("Warning:"+fmt, x...)
 }
 
 func Error(x ...interface{}) {
+	Errorf("%s\n", x...)
 }
 
-func Errorf(x ...interface{}) {
+func Errorf(fmt string, x ...interface{}) {
+	msg("Error:"+fmt, x...)
 }
 
 func Fatal(x ...interface{}) {
+	Fatalf("%s\n", x...)
 }
 
-func Fatalf(x ...interface{}) {
+func Fatalf(fmt string, x ...interface{}) {
+	msg("Fatal:"+fmt, x...)
+	os.Exit(1)
+}
+
+func msg(ff string, x ...interface{}) {
+	// TODO - log date time - find the config stuff in func()init - commented out and implement it.
+	// TODO - Destination for logging
+	// TODO - Location called from
+	fmt.Printf(ff, x...)
 }
